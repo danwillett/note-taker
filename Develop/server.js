eb6b31f28bf306a9f34798ebf0e2f84b35847e63
danwill <daniel.willett2@gmail.com>
2023-01-17 13:29:57 -0800

    heroku test

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 // const getNotes = require('./public/assets/js/index')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const api = require('./routes/index')
 
